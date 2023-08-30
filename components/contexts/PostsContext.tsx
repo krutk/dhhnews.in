2545e173cecb4f12fe0969f8f89f3a851d24806b
Kumar Utkarsh <utkarshroy28@gmail.com>
2023-08-31 04:47:37 +0530

    server-side fetching posts

--- components/contexts/PostsContext.tsx
+++ components/contexts/PostsContext.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
+// import React, { createContext, useContext, useEffect, useState } from "react";
+// import axios from "axios";
 
 type Post = {
   id: string;
@@ -24,6 +24,51 @@ type Post = {
   };
 };
 
+// type PostsContextType = {
+//   posts: Post[];
+//   loading: boolean;
+//   error: any;
+// };
+
+// const PostsContext = createContext<PostsContextType>({
+//   posts: [],
+//   loading: true,
+//   error: null,
+// });
+
+// export const usePostsContext = () => useContext(PostsContext);
+
+// const PostsProvider: any = ({ children }: any) => {
+//   const [posts, setPosts] = useState<Post[]>([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState<any>(null);
+
+//   useEffect(() => {
+//     axios
+//       .get("/api/posts")
+//       .then((response) => {
+//         setPosts(response.data);
+//         setLoading(false);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching posts:", error);
+//         setError(error);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   return (
+//     <PostsContext.Provider value={{ posts, loading, error }}>
+//       {children}
+//     </PostsContext.Provider>
+//   );
+// };
+
+// export default PostsProvider;
+
+import React, { createContext, useContext, useState } from "react";
+import axios from "axios";
+
 type PostsContextType = {
   posts: Post[];
   loading: boolean;
@@ -38,24 +83,34 @@ const PostsContext = createContext<PostsContextType>({
 
 export const usePostsContext = () => useContext(PostsContext);
 
-const PostsProvider: any = ({ children }: any) => {
+export async function getServerSideProps() {
+  try {
+    const response = await axios.get("/api/posts");
+    const posts = response.data;
+
+    return {
+      props: {
+        posts,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return {
+      props: {
+        posts: [],
+        error: "Error fetching posts",
+      },
+    };
+  }
+}
+
+const PostsProvider: React.FC = ({ children }: any) => {
+  // This can be an empty array as the data will be fetched using getServerSideProps
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<any>(null);
 
-  useEffect(() => {
-    axios
-      .get("/api/posts")
-      .then((response) => {
-        setPosts(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching posts:", error);
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
+  // No need for the useEffect here
 
   return (
     <PostsContext.Provider value={{ posts, loading, error }}>
